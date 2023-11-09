1ea065cbe5494a5c723724e4ebd21404e3aae3db
Maxim <146652883+Maxim100010@users.noreply.github.com>
2023-11-09 23:23:06 +0100

    Finish game and board classes
    Finished game class which can now run and rerun full games
    The board class was extended with tracking which fields were already played
    and a method to update the board representation was added

--- board.rb
+++ board.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class Board
-  attr_accessor :board, :board_representation, :winner
+  attr_accessor :board, :board_representation, :winner, :played_moves
 
   attr_reader :win_combinations
 
+  # Takes no arguments
+  # Creates the board and its visual representation
+  # Creates array of winning combinations
+  # Creates boolean keeping track of whether there is a winner or not
+  # Creates array of moves that were already played
   def initialize
     @board = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
     @board_representation = " #{board[0][0]} | #{board[0][1]} | #{board[0][2]} " + "\n" \
@@ -23,20 +28,38 @@ class Board
       [6, 4, 2] # right_diagonal
     ]
     @winner = false
+    @played_moves = []
   end
 
+  # Checks if the move is between 1 and 9 and if move was already played
+  # If the check is passed, replaces the number on the board with the symbol assigned to the player
+  # Also adds move to played moves
   def register_move(position, symbol)
-    0 if position != (1..9)
-    board.each do |x|
-      puts x
-      x.map { |y| y == position.to_s ? symbol : nil }
+    return 0 unless position.between?(1, 9) && !played_moves.include?(position)
+
+    board.each_with_index do |x, i|
+      x.each_with_index do |y, j|
+        (board[i][j] = symbol) && played_moves.push(position) if y == position.to_s
+      end
     end
     1
   end
 
+  # Checks if there is a winner, called after each round
+  # Checks if any combination currently on board matches any combination in the array of winning combinations
   def has_winner?
     winmap = win_combinations.map { |w| board.flatten.values_at(*w) }
-    pp winmap
-    winner = true if winmap.any? { |combination| combination.all?('X') || combination.all?('O') }
+    @winner = true if winmap.any? { |combination| combination.all?('X') || combination.all?('O') }
+  end
+
+  # Updates the board
+  # This method is necessary given that each time a move is made a new board is made in the memory
+  # This means that if we call the board representation it calls the representation of the original board
+  def update_board(updated_board)
+    @board_representation = " #{updated_board[0][0]} | #{updated_board[0][1]} | #{updated_board[0][2]} " + "\n" \
+                            '---+---+---' + "\n" \
+                            " #{updated_board[1][0]} | #{updated_board[1][1]} | #{updated_board[1][2]} " + "\n" \
+                            '---+---+---' + "\n" \
+                            " #{updated_board[2][0]} | #{updated_board[2][1]} | #{updated_board[2][2]} "
   end
 end
--- game.rb
+++ game.rb
@@ -11,6 +11,8 @@ class Game
     @board = nil
   end
 
+  # Gets the names of the players
+  # Creates Player and Board objects and assigns them to the instance variables
   def prepare_game
     name1 = ''
     name2 = ''
@@ -25,21 +27,36 @@ class Game
     self.player1 = Player.new(name1, 'X')
     self.player2 = Player.new(name2, 'O')
     self.board = Board.new
+    puts
     puts "Player #{player1.name}[X] VS Player #{player2.name}[O]"
   end
 
+  # Registers the players move and if valid returns true else returns false
   def player_move(player)
+    puts
+    puts "-- #{player.name}'s [#{player.symbol}] turn --"
     puts 'Choose a position to play (Number between 1 and 9)'
     move = gets
-    true if board.register_move(move.to_i, player.symbol) == 1
+    return true if board.register_move(move.to_i, player.symbol) == 1
+
+    false
   end
 
+  # Randomizes the starting player
+  # Keeps track of whose turn it is and the number of turns played
+  # Loops until there is a winner or the number of turns equals 8
+  # The loop updates and display the board, and depending on whose turn it is lets players input their move
+  # After the loop announces if the game was a draw or announces winning player
+  # The winning player is chosen by having their turn equal to false given that if a player plays a winning move their turn would be false
+  # Asks whether the player want to play a new game and recursively calls the method
   def play_game
     starting_player = rand(1..2)
     player1_turn = starting_player == 1
     player2_turn = starting_player == 2
-    puts player1_turn.to_s + ' ' + player2_turn.to_s
-    until board.winner
+    number_of_turns = 0
+    until board.winner || number_of_turns == 8
+      board.update_board(board.board)
+      puts
       puts board.board_representation
       if player1_turn == true
         a = false
@@ -52,7 +69,28 @@ class Game
         player2_turn = false
         player1_turn = true
       end
+      board.winner = board.has_winner?
+      number_of_turns += 1
     end
+    board.update_board(board.board)
+    puts
+    puts board.board_representation
+    if number_of_turns == 8
+      puts "It's a draw"
+      puts
+    elsif player2_turn == false
+      puts "The winner is Player 2 - '#{player2.name}' [#{player2.symbol}]"
+      puts
+    elsif player1_turn == false
+      puts "The winner is Player 1 - '#{player1.name}' [#{player1.symbol}]"
+      puts
+    end
+    puts 'Play again? (y/n)'
+    decision = gets
+    return unless decision.casecmp('y') == 1
+
+    prepare_game
+    play_game
   end
 end
 
